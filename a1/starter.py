3d74c68708bb52868eb5b0b1622b4bd355cf93b8
marujeyakumar <maru.jeyakumar@mail.utoronto.ca>
2019-01-20 14:32:14 -0500

    Another mistake with gradMSE

--- a1/starter.py
+++ a1/starter.py
@@ -44,7 +44,7 @@ def gradMSE(W, b, x, y, reg):
     for i in range(N): 
         X_sliced = x[i,:,:]
         X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
-        mse_gradient = (((np.matmul((W),np.transpose(X_sliced)) * X_sliced) + b)-y[i])
+        mse_gradient += (((np.matmul((W),np.transpose(X_sliced)) * X_sliced) + b)-y[i])
     
     mse_gradient *= 1/N 
     mse_gradient = reg * np.matmul(W, np.transpose(W))
