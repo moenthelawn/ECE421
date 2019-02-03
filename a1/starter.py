0edd0b6bb54dcd19e523438e7d71dc4897ea7f99
Chris Moen <moenchris1@gmail.com>
2019-02-03 09:55:39 -0500

    making changes

--- a1/starter.py
+++ a1/starter.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.misc
+from sklearn.utils import shuffle
 def loadData():
     with np.load('notMNIST.npz') as data :
         Data, Target = data ['images'], data['labels']
@@ -22,36 +23,24 @@ def loadData():
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
 def MSE(W, b, x, y, reg):
-    # Your implementation here
-        
-    N = len(y)
-    Total = 0
-    i = 0
-    #You wanna sum the inside and then multiply 1/2N after the loop exits 
-    for i in range(N):
-        X_sliced = x[i,:,:]
-        X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
-        
-        Total += np.square(((np.matmul((W),np.transpose(X_sliced)) + b)) - y[i])
+
+    y_pred = np.matmul(x,np.transpose(W)) + b
+    mse = (1/2) * np.mean(np.square(y_pred - y))
+    return (mse + (reg/2) * np.matmul(W, np.transpose(W)))
+
+def meanSquareError_normalWeights(W,X,y): 
+    Total *= (1/(2*N)) 
+    Total += (reg/2) * np.matmul(W, np.transpose(W))
+    return Total 
+
+def meanSquareError(W,x,y): 
 
     Total *= (1/(2*N)) 
     Total += (reg/2) * np.matmul(W, np.transpose(W))
     return Total[0][0]
-def MSE_Normal(W, b, x, y, reg):
-    # Your implementation here
-        
-    N = len(y)
-    Total = 0
-    i = 0
-    #You wanna sum the inside and then multiply 1/2N after the loop exits 
-    for i in range(N):
-        X_sliced = x[i,:,:]
-        X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
-        Total += np.square(((np.matmul((W),np.transpose(X_sliced)) + b)) - y[i])
 
-    Total *= (1/(N)) 
-    return Total 
 def meanSquareError_normalWeights(W,X,y): 
+
     #This function returns the minimized weights for the mean square 
     x = X.reshape(np.shape(X)[0],-1)
     biases = np.ones((np.shape(y)))
@@ -64,22 +53,20 @@ def meanSquareError_normalWeights(W,X,y):
     return wlin[:][1:],wlin[0][0]
     
 def gradMSE(W, b, x, y, reg):
-    N = len(y)
-    mse_gradient_weights = np.zeros(np.shape(W)) #Matrix to hold the gradients wrt weights 
-    mse_gradient_biases = 0
-    #You wanna sum the inside and then multiply 1/N after the loop exits 
-    for i in range(N): 
-        
-        X_sliced = x[i,:,:]
-        X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
-        mse_gradient_weights += np.dot((((np.matmul((W), np.transpose(X_sliced))) + b)-y[i]),X_sliced) 
-        mse_gradient_biases += (((np.matmul((W), np.transpose(X_sliced))) + b)-y[i]) 
-        
-    mse_gradient_weights *= 1/N 
-    mse_gradient_weights += reg * W
+
     
-    mse_gradient_biases *= 1/N
-    mse_gradient_biases += reg * W
+   # mse_gradient_weights = np.zeros(np.shape(W)) #Matrix to hold the gradients wrt weights 
+    #mse_gradient_biases = 0
+    N = len(y)
+    mse_gradient_weights = np.random.normal(0,1,np.shape(W)) #Matrix to hold the gradients wrt weights 
+    mse_gradient_biases = np.random.normal(0,1,np.shape(W))#Declare a random set of matrix values for the biases 
+    
+        
+    y_pred = np.matmul(x,np.transpose(W)) + b
+    yinside = y_pred - y
+    n = len(yinside)
+    mse_gradient_weights = (1/n) * np.matmul(np.transpose(yinside),x) + (reg) * W
+    mse_gradient_biases = np.mean(yinside)
     #mse_gradient_biases += reg * W
     
     return mse_gradient_weights,mse_gradient_biases
@@ -89,61 +76,54 @@ def getSign(number):
         return 1 
     else: 
         return 0
-    
+
     
 def accuracy(x,W,b,y): 
-    #This function will return the correct accuracy of a classifier 
-    N = len(y)
-    correctPrediction = 0 
+
+
+    y_hat = np.matmul(W,np.transpose(x))
+    accuracy = 0 
+    N =np.shape(y)[0]
+    
     for i in range(N): 
-        X_sliced = x[i,:,:]
-        X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
         y_hat = getSign(np.matmul(W,np.transpose(X_sliced)))
         y_expected = y[i]   
-        
-        if y_hat == y_expected: 
-            correctPrediction += 1
-            
-    accuracy = 100 * (correctPrediction / N)
-    return accuracy
-        
-def grad_descent_NormalEquation(W,trainingData,trainingLabels): 
-       
+
+        if y_hat_sign == ylabel: 
+            accuracy += 1
+    return 100*(accuracy/N)
+
+def grad_descent_NormalEquation(W,trainingData,trainingLabels):
    W_t,b = meanSquareError_normalWeights(W,trainingData,trainingLabels)       
    return np.transpose(W_t), b
-    
+
 def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
     #Added the ability to loop through and 
-   
-    j = 0 
+
     error_history = []
+    accuracy_history = []
+    
     for i in range(iterations):
-        mse = MSE(W,b,trainingData,trainingLabels,reg)
-        error_history.append(mse) 
+        X_flattened = trainingData.reshape(np.shape(trainingData)[0],-1)
+        mse = MSE(W,b,X_flattened,trainingLabels,reg)
+        
         if mse <= EPS: 
             break 
-        mse_gradient_weights, mse_gradient_biases = gradMSE(W,b,trainingData, trainingLabels,reg)
+        mse_gradient_weights, mse_gradient_biases = gradMSE(W,b,X_flattened, trainingLabels,reg)
         
         W -= mse_gradient_weights*alpha 
         b -= mse_gradient_biases*alpha
-    # mse = MSE(np.transpose(grad_descent),b,trainingData,trainingLabels,_reg)
-   # f = plt.figure(j)
-  #  accuracy(trainingData,W,b,trainingLabels)
-   # string_plot = "Learning Rate of " + str(_reg) + " with error " + str(mse[-1])
-    
-    # plt.plot(error_history) 
-    #plt.xlabel('Iterations')
-    #plt.ylabel('Error')
-   # plt.title (string_plot)
-   # f.show()
-    #plt.savefig(str(j))
-    
-    #print(mse[-1])
-    #j += 1
-    return W,b
+        accuracy_current= accuracy(X_flattened,W,b,trainingLabels) 
+        accuracy_history.append(accuracy_current) 
+        error_history.append(mse[0][0]) 
+        
+    return error_history,accuracy_history
+
 def crossEntropyLoss(W, b, x, y, reg):
     total = 0
     N = len(y)
+
+    
     for i in range(N):  
         X_sliced = x[i,:,:]
         X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
@@ -156,6 +136,7 @@ def crossEntropyLoss(W, b, x, y, reg):
     print(total)
     return total 
     
+
 def sigmoid(W, x, b):
     sigmoid_output = 0 
     raised_term = np.matmul(W, np.transpose(x)) + b
@@ -163,6 +144,128 @@ def sigmoid(W, x, b):
     sigmoid_output = 1 / (1 + exponential_term)
     
     return sigmoid_output 
+def gradCE(W, b, x, y, reg):
+    grad_ce_weights = 0 
+    grad_ce_biases = 0
+    N = len(y)
+    #Note the gradient with respect to the weights is exactly the same as the 
+    #gradient wrt to W of the MSE. 
+    for i in range(N):  
+        X_sliced = x[i,:,:]
+        X_sliced = np.reshape(X_sliced, (1,np.product(X_sliced.shape)))
+        grad_ce_weights += np.dot((sigmoid_value - y[i]), X_sliced)
+        grad_ce_biases += sigmoid_value - y[i]
+        
+    grad_ce_weights*= 1/N
+    grad_ce_weights += reg * W 
+    grad_ce_biases *= 1/N
+    return grad_ce_weights, grad_ce_biases  
+def sigmoid(W, x, b):
+    sigmoid_output = 0 
+    raised_term = np.matmul(W, np.transpose(x)) + b
+    exponential_term = np.exp(-raised_term) 
+    sigmoid_output = 1 / (1 + exponential_term)
+    return sigmoid_output
+    
+def buildGraph(loss=None):
+    #Initialize weight and bias tensors
+    tf.set_random_seed(421)
+
+    #Tensors to hold the bias and matrix values 
+    W = tf.Variable(tf.truncated_normal(mean= 0.0, shape = (1,784), stddev = 0.5, dtype = tf.float32, name = "weight"))
+    b = tf.Variable(1.0, name='biases', dtype=tf.float32)
+   
+    #Tensors to hold the variables 
+    X = tf.placeholder(tf.float32,[None, 784],name='X')
+    Y = tf.placeholder(tf.float32, [None, 1], name = 'Y')
+    reg = tf.constant(0, tf.float32, name = 'lambda') 
+    y_predicted = tf.matmul(X,tf.transpose(W)) + b
+    regParamater = ((reg/2) * tf.matmul(W,tf.transpose(W)))
+    
+    if loss == "MSE":
+    #Mean square error formula 
+        error = (1/2) * tf.reduce_mean(tf.reduce_mean(tf.squared_difference(y_predicted, Y))) + regParamater 
+        optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+        train = optimizer.minimize(loss=error)
+        
+        return W, b,X, y_predicted, Y, error, train, regParamater 
+    
+    # Your implementation
+    elif loss == "CE":
+        
+        error = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y,logits=y_predicted,name=None)) + regParamater
+     #   3q = tf.sigmoid(y_predicted, name = 'sigmoid') 
+      #  p = tf.reduce_sum((-1.0 * y_predicted * tf.log(q)) - ((1.0 - y_predicted) * tf.log(1.0 - q)))
+   
+        #error = tf.reduce_mean(p) + regParamater
+        optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+        train = optimizer.minimize(loss=error) #Adding the cross entropy error
+        
+        return W, b, X,y_predicted, Y, error, train, regParamater 
+def trainTensorModel(epochs, inputData, outputLabels,typeError,batch_size): 
+    # Initialize session
+    W, b, X, y_predicted, Y,error,train, reg = buildGraph(typeError)
+
+    init = tf.global_variables_initializer()
+   
+    X_flattened_training = inputData.reshape(np.shape(inputData)[0],-1)
+    
+    sess = tf.InteractiveSession()
+    sess.run(init)
+    
+   
+     
+    totalError = []
+    totalAccuracy = []
+    i = 0 
+    for step in range(0, epochs):
+        instances = np.shape(inputData)[0]
+        totalBatches = int(instances / batch_size)
+        idx = np.random.permutation(len(inputData))
+        X_shuffled,Y_shuffled = X_flattened_training[idx], trainTarget[idx]
+        
+        i = 0
+        for k in range(totalBatches):
+            X_batch = X_shuffled[i:(i + batch_size),:] 
+            y_batch = Y_shuffled[i:(i + batch_size),:]
+            _, err, currentW, currentb, yhat = sess.run([train, error, W, b, y_predicted], feed_dict={X:X_batch, Y:y_batch})
+         
+            i = i + batch_size
+        yhat_sign = tf.sign(tf.sign(yhat)+ 1)
+        data_tf = tf.convert_to_tensor(y_batch, np.float32)
+        correct_prediction = tf.equal(data_tf, yhat_sign)
+        acc = 100 * tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+           
+        totalAccuracy.append(acc)
+        totalError.append(err[0][0])  
+        
+    print("Final Error", str(err[0][0])) 
+    print("Final Accuracy", str(acc)) 
+    
+    return totalError, sess.run([totalAccuracy])
+    
+def figPlot(figureNumber,array, title,yLabel): 
+    f = plt.figure(figureNumber)
+    plt.xlabel('Iterations')
+    title = title 
+    plt.title(title)  
+    plt.ylabel(yLabel)
+    labels =[]
+    for eachValue, eachData in array.items(): 
+        plt.plot(eachData) 
+        labels.append(eachValue) 
+      
+    plt.legend(labels, ncol = len(labels))
+    f.show()
+    plt.savefig(str(figureNumber))
+    
+        
+
+#def crossEntropyLoss(W, b, x, y, reg):
+#Your implementation
+    
+#def gradCE(W, b, x, y, reg):
+#Your implementation
 
 #def crossEntropyLoss(W, b, x, y, reg):
 #Your implementation
@@ -186,12 +289,22 @@ def sigmoid(W, x, b):
 #     # Your implementation here
 # =============================================================================
     
-trainData,validData,testData,trainTarget,validTarget,testTarget = loadData()
- 
-W = testData[0,:,:]
-W = np.reshape(W, (1,np.product(W.shape)))
-W_Normal, bias_Normal = grad_descent_NormalEquation(W,trainData,trainTarget)
+#error_1,accuracy_training = grad_descent(W,1,trainData,trainTarget,0.0001,5000,0,1e-6)
+#error_2,accuracy_training = grad_descent(W,1,trainData,trainTarget,0.001,5000,0,1e-6)
+#error_3,accuracy_training = grad_descent(W,1,trainData,trainTarget,0.005,5000,0,1e-6)
+
+#figPlot(3,{"Training" : accuracy_training}, "Error" ,"Error") 
+#figPlot(4,{"LR = 0.0001" : error_1,"LR = 0.001" : error_2,"LR = 0.005" : error_3}, "Training Data Error With Varying Learning Rates" ,"Error") 
+error_train1, accuracy_train1 = trainTensorModel(700,trainData, trainTarget,"MSE",500, 0.95)
+error_train2, accuracy_train2 = trainTensorModel(700,trainData, trainTarget,"MSE",5000.999)
+#error_train3, accuracy_train3 = trainTensorModel(700,trainData, trainTarget,"MSE",500)
+
+figPlot(1,{"BS=100" : error_train1, "BS=700" : error_train2, "BS=1750" : error_train3},"Error With Varying Batch Size","Error") #Creating a function to map out all of the graphs 
+figPlot(2,{"BS=100" : accuracy_train1[0], "BS=700" : accuracy_train2[0], "BS=1750" : accuracy_train3[0]},"Accuracy With Varying Batch Size","Accuracy") #Creating a function to map out all of the graphs 
+
+#gradCE(W,1,trainData, trainTarget, 0)
 #MSE(W,1,testData,testTarget,0.1)
+
 
 #mse_gradient_weights, mse_gradient_biases = gradMSE(W,1,testData,testTarget,0.1)
 
